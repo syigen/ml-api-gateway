b4da58f3527cb88d7202ff307223fdc39e712131
mithu0621 <mithunisk03@gmail.com>
2025-01-03 17:26:04 +0530

    err:login authentication isn't completed yet

--- app/schemas/schemas.py
+++ app/schemas/schemas.py
@@ -1,4 +1,4 @@
-from pydantic import BaseModel, EmailStr
+from pydantic import BaseModel, EmailStr, constr
 
 class AuthRequest(BaseModel):
     email: EmailStr
@@ -8,3 +8,11 @@ class AuthResponse(BaseModel):
     id: int
     email: EmailStr
     api_key: str
+
+class UserCreate(BaseModel):
+    email: EmailStr
+    password: constr(min_length=8, pattern=r"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$")
+
+class UserResponse(BaseModel):
+    id: int
+    email: str
