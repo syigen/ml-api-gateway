eb521eb3f91d3e6f104cf9ca00f319b3fa374df8
SadeepaNHerath <sadeepahearth@gmail.com>
2025-01-03 17:38:45 +0530

    WIP: user registration feature

---
+++ app/api/v1/endpoints/user_register.py
@@ -0,0 +1,18 @@
+from sqlalchemy.orm import Session
+from fastapi import Depends, APIRouter, HTTPException
+
+from app.db.database import get_db
+from app.schemas.user_create import UserCreate, RegisterResponse
+from app.services.user_service import create_user
+
+router = APIRouter()
+
+@router.post("/register", response_model=RegisterResponse)
+def register_user(user: UserCreate, db: Session = Depends(get_db)):
+    try:
+        created_user = create_user(user, db)
+        return RegisterResponse(email=created_user.email, message="User  registered successfully.")
+    except ValueError as e:
+        raise HTTPException(status_code=400, detail=str(e))
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
