d729483d936249ed11e53cd25b16aa6d4bc16f0d
SadeepaNHerath <sadeepahearth@gmail.com>
2025-01-03 17:33:24 +0530

    fix: datatype of created_at

--- app/db/models.py
+++ app/db/models.py
@@ -1,4 +1,6 @@
-from sqlalchemy import Column, Integer, String
+from datetime import datetime
+
+from sqlalchemy import Column, Integer, String, DateTime
 from .database import Base
 
 class User(Base):
@@ -7,4 +9,4 @@ class User(Base):
     id = Column(Integer, primary_key=True, index=True)
     email = Column(String, unique=True, index=True)
     hashed_password = Column(String)
-    created_at = Column(String)
+    created_at = Column(DateTime, default=datetime.now(), nullable=False)
