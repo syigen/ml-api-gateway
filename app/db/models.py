07e052ba11bc4a7e22f52eedcb07b4c8e3d3f54e
IroshanRathnayake <meet.iroshan@gmail.com>
2025-01-03 17:31:03 +0530

    WIP: Implement API key generation, hashing, and verification in APIKeyManager.

--- app/db/models.py
+++ app/db/models.py
@@ -1,4 +1,8 @@
-from sqlalchemy import Column, Integer, String
+from datetime import datetime
+
+from sqlalchemy import Column, Integer, String, ForeignKey, DateTime
+from sqlalchemy.orm import relationship
+
 from .database import Base
 
 class User(Base):
@@ -8,3 +12,14 @@ class User(Base):
     email = Column(String, unique=True, index=True)
     hashed_password = Column(String)
     created_at = Column(String)
+
+class UserAPIKeys(Base):
+    __tablename__ = "user_api_keys"
+
+    id = Column(Integer, primary_key=True, index=True)
+    user_id = Column(Integer, ForeignKey("users.id", ondelete="CASCADE"), nullable=False)
+    api_key = Column(String(255), nullable=False)
+    created_at = Column(DateTime, default=datetime.now(), nullable=False)
+    updated_at = Column(DateTime, default=datetime.now(), onupdate=datetime.now(), nullable=False)
+
+    user = relationship("User", back_populates="api_keys")
