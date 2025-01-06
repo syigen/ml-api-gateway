abb119e51ee5cdb2f3e3d83d84e53b00dca045c4
mithu0621 <mithunisk03@gmail.com>
2025-01-06 16:48:22 +0530

    feat:user login authentication

--- app/db/models.py
+++ app/db/models.py
@@ -1,17 +1,27 @@
 from datetime import datetime
 
 from sqlalchemy import Column, Integer, String, ForeignKey, DateTime
-from sqlalchemy.orm import relationship
 
 from .database import Base
 
 class User(Base):
+    """
+        Represents a user in the database.
+
+        Attributes:
+        - __tablename__ (str): The name of the database table.
+        - id (Column): The user's ID.
+        - email (Column): The user's email address.
+        - hashed_password (Column): The user's hashed password.
+        - created_at (Column): The timestamp when the user was created.
+    """
     __tablename__ = "users"
 
     id = Column(Integer, primary_key=True, index=True)
     email = Column(String, unique=True, index=True)
     hashed_password = Column(String)
     created_at = Column(DateTime, default=datetime.now(), nullable=False)
+
 
 class UserAPIKeys(Base):
     __tablename__ = "user_api_keys"
--- app/schemas/schemas.py
+++ app/schemas/schemas.py
@@ -17,10 +17,26 @@ class UserResponse(BaseModel):
     id: int
     email: str
 
+
 class AuthRequest(BaseModel):
+    """
+        Represents the user's login credentials.
+
+        Attributes:
+        - email (EmailStr): The user's email address.
+        - password (str): The user's password.
+    """
     email: EmailStr
     password: str
 
+
 class AuthResponse(BaseModel):
+    """
+        Represents the authenticated user's details.
+
+        Attributes:
+        - id (int): The user's ID.
+        - email (EmailStr): The user's email address.
+    """
     id: int
     email: EmailStr
