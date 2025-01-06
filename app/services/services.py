9918da124c6b2d4de83496108b1f61d1d1b9ff31
mithu0621 <mithunisk03@gmail.com>
2025-01-06 11:25:43 +0530

    feat:login authentication bugs fixed

--- app/services/services.py
+++ app/services/services.py
@@ -1,11 +1,13 @@
+from pydantic import EmailStr
 from sqlalchemy.orm import Session
 from app.db.models import User
 from passlib.context import CryptContext
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
-def verify_user(email: str, password: str, db: Session):
-    user = db.query(User).filter(User.email == email).first()
-    if user and pwd_context.verify(password, user.password):
-        return user
-    return None
+def verify_user(email: EmailStr, password: str, db: Session):
+    email_user = db.query(User).filter(User.email == email).first()
+    if email_user and pwd_context.verify(password, email_user.password):
+        email_user.password = None
+        return email_user
+    raise ValueError("Password does not match!")
