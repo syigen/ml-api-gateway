1a912cf2f8e1c58ac21629a60a56d48db66f2a2e
mithu0621 <mithunisk03@gmail.com>
2025-01-06 11:54:00 +0530

    feat:login authentication verify user method changed

--- app/services/services.py
+++ app/services/services.py
@@ -1,13 +1,14 @@
-from pydantic import EmailStr
 from sqlalchemy.orm import Session
 from app.db.models import User
 from passlib.context import CryptContext
 
+from app.schemas.schemas import UserLogin
+
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
-def verify_user(email: EmailStr, password: str, db: Session):
-    email_user = db.query(User).filter(User.email == email).first()
-    if email_user and pwd_context.verify(password, email_user.password):
+def verify_user(user:UserLogin, db: Session):
+    email_user = db.query(User).filter(User.email == user.email).first()
+    if email_user and pwd_context.verify(str(user.email), user.password):
         email_user.password = None
         return email_user
     raise ValueError("Password does not match!")
