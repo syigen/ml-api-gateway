9bcd8fa82152e8485fce67c7b4aa092a182b9dca
shamax1999 <shamax199@gmail.com>
2025-01-06 16:05:51 +0530

    fix:add router path for the main

--- main.py
+++ main.py
@@ -1,15 +1,9 @@
 from fastapi import FastAPI
 from app.api.v1.endpoints.base import router as base_router
-from app.api.v1.endpoints.user_login import router as login_router
-from app.api.v1.endpoints.user_register import router as register_router
-from app.db.database import Base, engine
 
 app = FastAPI()
-Base.metadata.create_all(bind=engine)
 
 app.include_router(base_router, prefix="/api/v1", tags=["base"])
-app.include_router(login_router, prefix="/api/v1/user", tags=["user_login"])
-app.include_router(register_router, prefix="/api/v1/user", tags=["user_register"])
 
 if __name__ == '__main__':
     import uvicorn
