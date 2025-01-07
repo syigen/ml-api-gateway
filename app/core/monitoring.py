9edd9ff8871ea41b4acacf5d5b83dab4c0ae97a8
shamax1999 <shamax199@gmail.com>
2025-01-07 09:07:46 +0530

    feat: Implemented threading to handle response time measurement.

--- app/core/monitoring.py
+++ app/core/monitoring.py
@@ -1,8 +1,15 @@
 import time
-import httpx
 from fastapi import APIRouter
+from fastapi.testclient import TestClient
+import threading
+from fastapi import FastAPI
 
+app = FastAPI()
 router = APIRouter()
+
+response_time_data = {}
+
+app.include_router(router)
 
 
 @router.get("/response", tags=["Response"])
@@ -11,29 +18,21 @@ async def get_responses():
 
 
 @router.get("/response-usage", tags=["ResponseUsage"])
-async def log_response_usage():
+def trigger_response_usage():
+    def measure_response_time():
+        with TestClient(app) as client:
+            start_time = time.time()
 
-    response_api_url = "http://localhost:8000/api/v1/response"
+            response = client.get("/response")
 
-    start_time = time.time()
-
-    async with httpx.AsyncClient() as client:
-        try:
-            response = await client.get(response_api_url, timeout=10.0)
             end_time = time.time()
-
             response_time = end_time - start_time
 
+            response_time_data["/response"] = response_time
             print(f"Response time for /response: {response_time:.4f} seconds")
 
-            return {
-                "message": "Logged response time successfully",
-                "response_time": response_time,
-                "status_code": response.status_code
-            }
-        except httpx.RequestError as e:
 
-            print(f"Failed to call /response: {e}")
-            return {"message": "Failed to call /response", "error": str(e)}
+    thread = threading.Thread(target=measure_response_time)
+    thread.start()
 
-#print()
+    return {"message": "Response usage calculation triggered."}
