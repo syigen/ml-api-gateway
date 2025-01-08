e3570ef3206ce3c6f4f2bf7c29973e11a8c957ac
IroshanRathnayake <meet.iroshan@gmail.com>
2025-01-08 17:41:50 +0530

    test: implement test case for reset API key endpoint

--- tests/test_reset_api_key.py
+++ tests/test_reset_api_key.py
@@ -1,27 +1,57 @@
+import pytest
 from fastapi.testclient import TestClient
-from unittest.mock import MagicMock, patch
-from sqlalchemy.orm import Session
 
-from app.db.models import User
-from app.schemas.schemas import AuthRequest
+from app.db.database import get_db
 from main import app
+from .test_database import db_session, test_key_user, UserAPIKeys, TestingSessionLocal
 
 
-def test_reset_api_key():
+@pytest.fixture
+def client():
+    def override_get_db():
+        db = TestingSessionLocal()
+        try:
+            yield db
+        finally:
+            db.close()
 
-    client = TestClient(app)
+    app.dependency_overrides[get_db] = override_get_db
+    yield TestClient(app)
 
-    user = User(id=1, email="test@email.com")
-    login_data = AuthRequest(email="test@email.com", password="test_password")
 
-    with patch("app.api.v1.deps.validate_api_key", return_value=user):
-        with patch("app.db.database.get_db", return_value=MagicMock(spec=Session)):
-            with patch("app.core.security.APIKeyManager.reset_key", return_value="new_generated_key"):
+def test_reset_api_key(db_session, test_key_user, client):
+    retrieved_api_key = db_session.query(UserAPIKeys).filter_by(user_id=test_key_user.id).first()
+    assert retrieved_api_key, "API key not found for the user in the database"
+    print(f"Retrieved API Key: {retrieved_api_key.api_key}")
 
-                response = client.post(
-                    "/api/v1/user/reset-api-key",
-                    json=login_data.model_dump(),
-                    headers={"x-api-key": "new_generated_key"}
-                )
+    login_data = {
+        "email": test_key_user.email,
+        "password": "User@123"
+    }
 
-                assert response.status_code == 401
+    response = client.post(
+        "api/v1/user/reset-api-key",
+        json=login_data,
+        headers={
+            "x-api-key": retrieved_api_key.api_key,
+            'Content-Type': 'application/json'
+        }
+    )
+
+    print(f"Response status: {response.status_code}")
+    print(f"Response body: {response.json()}")
+
+    assert response.status_code == 200
+    response_data = response.json()
+    assert response_data["email"] == test_key_user.email
+    assert response_data["api_key"] != retrieved_api_key.api_key
+    assert response_data["message"] == "Your previous API key will expire in 5 minutes."
+
+    new_api_key = response_data["api_key"]
+    new_key_record = db_session.query(UserAPIKeys).filter_by(api_key=new_api_key).first()
+    assert new_key_record, "New API key not found in the database"
+    assert new_key_record.user_id == test_key_user.id
+
+    db_session.delete(new_key_record)
+    db_session.delete(test_key_user)
+    db_session.commit()
