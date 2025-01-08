3979fce523c7f4049df4e8b986a6ef310d29c3e5
shamax1999 <shamax199@gmail.com>
2025-01-08 14:49:13 +0530

    feat: Implement response time monitoring with decorator and finalized test cases for changes.

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from app.api.v1.endpoints.base import router as base_router
 from app.api.v1.endpoints.user_login import router as login_router
 from app.api.v1.endpoints.user_register import router as register_router
 from app.db.database import Base, engine
+from app.api.v1.endpoints.monitor import router as monitoring_router
+
 
 app = FastAPI()
 Base.metadata.create_all(bind=engine)
@@ -11,6 +13,7 @@ app.include_router(base_router, prefix="/api/v1", tags=["base"])
 app.include_router(base_router, prefix="/core", tags=["monitoring"])
 app.include_router(login_router, prefix="/api/v1/user", tags=["user_login"])
 app.include_router(register_router, prefix="/api/v1/user", tags=["user_register"])
+app.include_router(monitoring_router, prefix="/api/v1/monitor", tags=["monitor"])
 
 if __name__ == '__main__':
     import uvicorn
