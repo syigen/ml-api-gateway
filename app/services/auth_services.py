139e428d95fffd37feca083745dc1b27880f9a6b
SadeepaNHerath <sadeepahearth@gmail.com>
2025-01-16 16:40:07 +0530

    refactor:integrate services

--- app/services/auth_services.py
+++ app/services/auth_services.py
@@ -1,11 +1,11 @@
 from typing import Type
 
 from fastapi import HTTPException
-from sqlalchemy.orm import Session
-from app.db.models import User, UserAPIKeys
 from passlib.context import CryptContext
+from sqlalchemy.orm import Session
 
-from app.schemas.schemas import AuthRequest
+from app.db.models import User, UserAPIKeys
+from app.schemas.auth_schemas import AuthRequest
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
@@ -31,20 +31,8 @@ def verify_user(user: AuthRequest, db: Session):
         return email_user
     raise ValueError("Credentials do not match!")
 
+
 def get_user_api_key(user_id: int, db: Session) -> Type[UserAPIKeys]:
-    """
-     Retrieve the API key for a specific user.
-
-     Args:
-         user_id (int): The ID of the user.
-         db (Session): The database session to execute queries.
-
-     Returns:
-         UserAPIKeys: The API key object associated with the user.
-
-     Raises:
-         HTTPException: If no API key is found for the user (401 Unauthorized).
-     """
     user_api_key = db.query(UserAPIKeys).filter(UserAPIKeys.user_id == user_id).first()
     if not user_api_key:
         raise HTTPException(status_code=401, detail="Invalid API Key")
