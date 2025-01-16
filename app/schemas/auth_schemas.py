c955eb6f136d402c9266c5ccd01c04a09928dd76
SadeepaNHerath <sadeepahearth@gmail.com>
2025-01-16 16:39:21 +0530

    integrate schemas

--- app/schemas/auth_schemas.py
+++ app/schemas/auth_schemas.py
@@ -1,21 +1,4 @@
-from pydantic import BaseModel, EmailStr, constr
-
-class AuthRequest(BaseModel):
-    email: EmailStr
-    password: str
-
-class AuthResponse(BaseModel):
-    id: int
-    email: EmailStr
-    api_key: str
-
-class UserCreate(BaseModel):
-    email: EmailStr
-    password: constr(min_length=8, pattern=r"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$")
-
-class UserResponse(BaseModel):
-    id: int
-    email: str
+from pydantic import BaseModel, EmailStr
 
 
 class AuthRequest(BaseModel):
@@ -40,3 +23,4 @@ class AuthResponse(BaseModel):
     """
     id: int
     email: EmailStr
+    api_key: str
